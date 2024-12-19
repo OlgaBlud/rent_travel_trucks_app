3a15e2396f89f058d6ae03e53c23239f689242fe
Olga Blud <olga.blud@gmail.com>
2024-12-20 01:14:23 +0200

    addCampers rewiews

--- src/components/CamperReviews/CamperReviews.jsx
+++ src/components/CamperReviews/CamperReviews.jsx
@@ -1,5 +1,51 @@
+import { useSelector } from "react-redux";
+import css from "./CamperReviews.module.css";
+import { selectCamperDetails } from "../../redux/campers/selectors";
+import { nanoid } from "nanoid";
+import icons from "../../assets/sprite.svg";
+
 function CamperReviews() {
-  return <div>CamperReviews</div>;
+  const camper = useSelector(selectCamperDetails);
+  const starsMax = 5;
+  const getStarColor = (index, rating) => {
+    if (index < rating) {
+      return "#ffc531"; // Цвет для заполненных звезд
+    }
+    return "#f2f4f7"; // Цвет для пустых звезд
+  };
+  return (
+    <div className={css.reviewsWrap}>
+      <ul className={css.reviewList}>
+        {camper.reviews.length === 0 ? (
+          <p>Sorry, there are no reviews!</p>
+        ) : (
+          camper.reviews.map((item) => (
+            <li className={css.reviewItem} key={nanoid()}>
+              <div className={css.reviewHead}>
+                <div className={css.reviewerName}>
+                  {item.reviewer_name.charAt(0)}
+                </div>
+                <div>
+                  <h3 className={css.reviewerNameFull}>{item.reviewer_name}</h3>
+                  {[...Array(starsMax)].map((_, index) => (
+                    <svg
+                      width="16"
+                      height="16"
+                      fill={getStarColor(index, item.reviewer_rating)}
+                      key={nanoid()}
+                    >
+                      <use href={`${icons}#star-1`} />
+                    </svg>
+                  ))}
+                </div>
+              </div>
+              <p className={css.reviewerComment}>{item.comment}</p>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default CamperReviews;
