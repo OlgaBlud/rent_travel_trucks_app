6a75239036707663d323ab94493138af9b914216
Olga Blud <olga.blud@gmail.com>
2024-12-21 18:31:38 +0200

    add persistor

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import { BrowserRouter } from "react-router";
 import App from "./components/App/App";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,55 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import { campersReducer } from "./campers/slice";
+// import { favoritesReducer } from "./favorites/slice";
+// import { filtersReducer } from "./filters/slice";
+
+// export const store = configureStore({
+//   reducer: {
+//     campers: campersReducer,
+//     favorites: favoritesReducer,
+//     filters: filtersReducer,
+//   },
+// });
+
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { campersReducer } from "./campers/slice";
 import { favoritesReducer } from "./favorites/slice";
 import { filtersReducer } from "./filters/slice";
 
+const favoritesPersistConfig = {
+  key: "favorites",
+  storage,
+};
+
+const filtersPersistConfig = {
+  key: "filters",
+  storage,
+};
+
 export const store = configureStore({
   reducer: {
     campers: campersReducer,
-    favorites: favoritesReducer,
-    filters: filtersReducer,
+    filters: persistReducer(filtersPersistConfig, filtersReducer),
+    favorites: persistReducer(favoritesPersistConfig, favoritesReducer),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
